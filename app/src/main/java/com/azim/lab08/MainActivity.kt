b8b2980ea9332ad0906a47210063ee22549d50bc
JaiKV <mazimzaidan@gmail.com>
2024-11-24 11:10:24 +0800

    update

--- app/src/main/java/com/azim/lab08/MainActivity.kt
+++ app/src/main/java/com/azim/lab08/MainActivity.kt
@@ -2,11 +2,14 @@ package com.azim.lab08
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.azim.lab08.databinding.ActivityMainBinding
+import com.google.android.material.slider.Slider
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,6 +17,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        lateinit var slider: SeekBar
+        lateinit var value: TextView
+
+        slider = binding.seekbar
+        value = binding.sizeTextView
+
+
 
         binding.scheduleButton.setOnClickListener {
             val intent = Intent(this,DetailActivity::class.java)
